8fa0a8b1e53588bb0c205312893b006501802f79
IgnacioCarvajal070 <ignacio.carvajal@alumnos.ucn.cl>
2024-05-06 15:02:08 -0400

    feat: Added GetChairByName

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using chairs_dotnet7_api;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,23 +16,57 @@ var chairs = app.MapGroup("api/chair");
 
 //TODO: ASIGNACION DE RUTAS A LOS ENDPOINTS
 chairs.MapGet("/", GetChairs);
-chairs.MapGet("/{id}", GetChairById);
-chairs.MapGet("/{name}", GetChairByName);
+chairs.MapGet("/{nombre}", GetChairByName);
 chairs.MapPost("/", addChair);
+chairs.MapPost("/chair/purchase", buyChair);
 chairs.MapPut("/{id}", updateChair);
 chairs.MapPut("/{id}/stock", updateOnlyStockChair);
-chairs.MapPost("/chair/purchase", buyChair);
 chairs.MapDelete("/{id}", deleteChair);
-
 app.Run();
 
 //TODO: ENDPOINTS SOLICITADOS
 async Task<IResult> GetChairs(DataContext db)
 {
-    return TypedResults.Ok();
+    List<Chair> chairlist = await db.Chairs.ToListAsync();
+    return TypedResults.Ok(chairlist);
+}
+
+async Task<Chair> GetChairById(int id, DataContext db)
+{
+    Chair? foundChair = await db.Chairs.FindAsync(id);
+    return foundChair;
+}
+
+async Task<IResult> GetChairByName(string nombre, DataContext db)
+{
+    Chair? foundChair = await db.Chairs.Where(u => u.Nombre == nombre).FirstOrDefaultAsync();
+    if(foundChair is null) return TypedResults.NotFound("no se encontró la silla con ese nombre");
+    return TypedResults.Ok(foundChair);
 }
 
 async Task<IResult> addChair(Chair chair)
 {
     return TypedResults.Ok();
 }
+
+async Task buyChair(HttpContext context)
+{
+    throw new NotImplementedException();
+}
+
+async Task updateChair(HttpContext context)
+{
+    throw new NotImplementedException();
+}
+
+async Task updateOnlyStockChair(HttpContext context)
+{
+    throw new NotImplementedException();
+}
+
+async Task deleteChair(HttpContext context)
+{
+    throw new NotImplementedException();
+}
+
+
