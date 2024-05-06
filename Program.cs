86be1f8724f4ddf44d7ebe92e1343f1b462bbb97
IgnacioCarvajal070 <ignacio.carvajal@alumnos.ucn.cl>
2024-05-06 14:40:55 -0400

    feat: Added mapping routes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using chairs_dotnet7_api;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,11 +15,23 @@ var chairs = app.MapGroup("api/chair");
 
 //TODO: ASIGNACION DE RUTAS A LOS ENDPOINTS
 chairs.MapGet("/", GetChairs);
+chairs.MapGet("/{id}", GetChairById);
+chairs.MapGet("/{name}", GetChairByName);
+chairs.MapPost("/", addChair);
+chairs.MapPut("/{id}", updateChair);
+chairs.MapPut("/{id}/stock", updateOnlyStockChair);
+chairs.MapPost("/chair/purchase", buyChair);
+chairs.MapDelete("/{id}", deleteChair);
 
 app.Run();
 
 //TODO: ENDPOINTS SOLICITADOS
-static IResult GetChairs(DataContext db)
+async Task<IResult> GetChairs(DataContext db)
+{
+    return TypedResults.Ok();
+}
+
+async Task<IResult> addChair(Chair chair)
 {
     return TypedResults.Ok();
 }
